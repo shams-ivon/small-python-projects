c18d7e48aeabfd690ab0777d9ff8bb10f336cc61
shams-ivon <sfurvdiorwen@gmail.com>
2023-12-10 22:03:44 +0600

    efficient starting point

---
+++ hirst-painting-gui/main.py
@@ -0,0 +1,43 @@
+from extracted_colors import color_tuple_list
+from turtle import Turtle, Screen
+import random
+
+upto = len(color_tuple_list) - 1
+
+screen = Screen()
+screen.colormode(255)
+
+arrow_turtle = Turtle()
+arrow_turtle.width(10)
+
+arrow_turtle.penup()
+arrow_turtle.left(225)
+arrow_turtle.forward(200)
+arrow_turtle.right(225)
+arrow_turtle.pendown()
+
+for i in range(10):
+    
+    color = color_tuple_list[random.randint(0, upto)]
+    arrow_turtle.color(color)
+    arrow_turtle.dot()
+
+    for j in range(9):
+
+        arrow_turtle.penup()
+        arrow_turtle.forward(40)
+        arrow_turtle.pendown()
+
+        color = color_tuple_list[random.randint(0, upto)]
+        arrow_turtle.color(color)
+        arrow_turtle.dot()
+
+    arrow_turtle.penup()
+    arrow_turtle.left(90)
+    arrow_turtle.forward(40)
+    arrow_turtle.left(90)
+    arrow_turtle.forward(9 * 40)
+    arrow_turtle.right(180)
+    arrow_turtle.pendown()
+        
+screen.exitonclick()
