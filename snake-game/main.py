47d153323a167e9cb0539475ad66fd27d2a6b1d4
Shams Ivon <shams919ivon@gmail.com>
2023-12-12 16:32:46 +0600

    tracer and update method used for seamless transition of the snake

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 snake_blocks = []
 snake_positions = [(0, 0), (-20, 0), (-40, 0)]
@@ -13,8 +15,16 @@ for index in range(3):
     block = Turtle("square")
     block.color("white")
     block.penup()
-    block.speed(0)
+    
     block.goto(snake_positions[index])
     snake_blocks.append(block)
+
+while True:
+
+    for block in snake_blocks:
+        block.forward(20)
+    
+    screen.update()
+    time.sleep(0.3)
 
 screen.exitonclick()
