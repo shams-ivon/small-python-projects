5d01080cfc47e5df453035b06c8ea94dbf6ee06d
shams-ivon <sfurvdiorwen@gmail.com>
2023-12-21 01:15:48 +0600

    paddles added

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,12 +1,34 @@
 from turtle import Screen
+from paddle import Paddle
+from field_decorator import Field_decorator
+
+LEFT_MARGIN = -370
+RIGHT_MARGIN = 370
+TOP_MARGIN = 250
+BOTTOM_MARGIN = -250
+LEFT_PADDLE_INIT_X = -350
+LEFT_PADDLE_INIT_Y = 0
+RIGHT_PADDLE_INIT_X = 350
+RIGHT_PADDLE_INIT_Y = 0
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-screen.tracer(0)
+# screen.tracer(0)
 screen.listen()
 
+field_decorator = Field_decorator()
+left_paddle = Paddle(LEFT_PADDLE_INIT_X, LEFT_PADDLE_INIT_Y)
+right_paddle = Paddle(RIGHT_PADDLE_INIT_X, RIGHT_PADDLE_INIT_Y)
 
+field_decorator.draw_border()
+
+# screen.update()
+
+screen.onkey(left_paddle.up, "q")
+screen.onkey(left_paddle.down, "a")
+screen.onkey(right_paddle.up, "o")
+screen.onkey(right_paddle.down, "l")
 
 screen.exitonclick()
--- pong-game/paddle.py
+++ pong-game/paddle.py
@@ -1,6 +1,28 @@
 from turtle import Turtle
 
+STRETCH_HOR = 1.5
+STRETCH_VER = 4.5
+TOP_MARGIN = 200
+BOTTOM_MARGIN = -200
+
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, goto_x, goto_y):
         super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_len=STRETCH_HOR, stretch_wid=STRETCH_VER)
+        self.goto(goto_x, goto_y)
+
+    def up(self):
+        move_up = min([100, TOP_MARGIN - self.ycor()])
+        cur_x = self.xcor()
+        cur_y = self.ycor()
+        self.goto(cur_x, cur_y + move_up)
+    
+    def down(self):
+        move_down = min([100, self.ycor() - BOTTOM_MARGIN])
+        cur_x = self.xcor()
+        cur_y = self.ycor()
+        self.goto(cur_x, cur_y - move_down)
