154c60650994b8db08c5dff398141fc8e0afa871
shams-ivon <shams919ivon@gmail.com>
2024-01-07 20:52:17 +0600

    data gets saved to local .txt file

--- password-manager/main.py
+++ password-manager/main.py
@@ -6,6 +6,18 @@ LABEL_FONT = ("Courier", 15)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("saved_passwords.txt", "a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+
+    website_entry.delete(0, "end")
+    password_entry.delete(0, "end")
+    website_entry.focus()
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -27,9 +39,11 @@ password_label = Label(text="Password :", font=LABEL_FONT)
 password_label.grid(row=4, column=1)
 
 website_entry = Entry(width=37)
+website_entry.focus()
 website_entry.grid(row=2, column=2, columnspan=2)
 
 email_entry = Entry(width=37)
+email_entry.insert(0, "abc@efg.com")
 email_entry.grid(row=3, column=2, columnspan=2)
 
 password_entry = Entry()
@@ -38,7 +52,7 @@ password_entry.grid(row=4, column=2, columnspan=1)
 generate_button = Button(text="Generate Password")
 generate_button.grid(row=4, column=3)
 
-add_button = Button(text="Add", width=35)
+add_button = Button(text="Add", width=35, command=save)
 add_button.grid(row=5, column=2, columnspan=2)
 
 window.mainloop()
